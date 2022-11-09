963f2aaf7404646f076d3939b2b701945cf14343
vjvl95 <vjvl95@naver.com>
2022-11-09 10:52:53 +0900

    solved 큐

--- 자바스크립트문제/백준문제/큐.js
+++ 자바스크립트문제/백준문제/큐.js
@@ -2,26 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname+"/input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-let result=[]
 function solution(){
+    let result=[]
     let num=+input[0]
+    let answer=[]
     for(let i=1;i<=num;i++){
-        input[i].split(" ").length===1?console.log(length_1(input[i])):console.log(length_2(input[i]))
-}
+        input[i].split(" ").length===1?answer.push(length_1(input[i])):length_2(input[i])
+    }
 function length_1(input){
+    input=input.trim()
     if(input==="size") return result.length
     if(input==="empty") return result.length===0?1:0
     if(input==="front") return result.length===0?-1:result[0]
     if(input==="back")  return result.length===0?-1:result[result.length-1]
-    if(input==="pop")   return result.length===0?-1:result.pop()
+    if(input==="pop")   return result.length===0?-1:result.shift()
 }
 function length_2(input){
     input=input.split(" ")
-    if(input[0]==="push"){
-        result.push(+input[1])
-        return +input[1]
-    }
+    result.push(+input[1])    
 }
+console.log(answer.join("\n"))
 }
 
 solution()
