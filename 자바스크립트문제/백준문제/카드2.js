b8c941ce9d9ec7101afecdd570cc75140c715ceb
vjvl95 <vjvl95@naver.com>
2022-11-09 21:24:40 +0900

    solved 카드2

--- 자바스크립트문제/백준문제/카드2.js
+++ 자바스크립트문제/백준문제/카드2.js
@@ -2,65 +2,15 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname+"/input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
-    }
-}
- 
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this._size = 0;
-    }
- 
-    add(value) {
-        const newNode = new Node(value);
- 
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            this.tail.next = newNode;
-            newNode.prev = this.tail;
-        }
- 
-        this.tail = newNode;
-        this._size++;
- 
-        return newNode;
-    }
- 
-    getHead() {
-        return this.head.value;
-    }
- 
-    removeHead() {
-        this.head = this.head.next;
-        this.head.prev = null;
-        this._size--;
-    }
- 
-    getSize() {
-        return this._size;
-    }
-}
-
 function solution(){
-    const cards=new LinkedList();
     let num=+input[0]
-
-    for(let i=1;i<=num;i++){
-        cards.add(i)
+    let tempArr=Array(num).fill(0).map((v,i)=>v=num-i)
+    while(tempArr.length!==1){
+        tempArr.pop()
+        let tempNum=tempArr.pop()
+        tempArr=[tempNum,...tempArr]
     }
-    while(cards.getSize()!==1){
-        cards.removeHead()
-        cards.add(cards.getHead())
-        cards.removeHead()
-    }
-    console.log(cards.getHead())
+    console.log(tempArr[0])
 }
 
 solution()
