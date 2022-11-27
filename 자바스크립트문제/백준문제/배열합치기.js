fa4939a5076c3d2ea319635537d6aa9429a19fb9
vjvl95 <vjvl95@naver.com>
2022-11-27 20:55:17 +0900

    refactor 배열합치기

--- 자바스크립트문제/백준문제/배열합치기.js
+++ 자바스크립트문제/백준문제/배열합치기.js
@@ -7,13 +7,7 @@ function solution() {
   arr1 = input[1].split(' ').map(Number);
   arr2 = input[2].split(' ').map(Number);
 
-  [...arr1, ...arr2]
-    .sort((a, b) => a - b)
-    .forEach((v, i) => {
-      if (i === 0) {
-      }
-      str += v + ' ';
-    });
+  [...arr1, ...arr2].sort((a, b) => a - b).forEach((v) => (str += v + ' '));
   console.log(str);
 }
 
