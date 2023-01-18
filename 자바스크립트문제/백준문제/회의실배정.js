24dca30c36f4068f66035036398e24ef174b6fed
vjvl95 <vjvl95@naver.com>
2023-01-19 00:00:39 +0900

    soveld 회의실배정

--- 자바스크립트문제/백준문제/회의실배정.js
+++ 자바스크립트문제/백준문제/회의실배정.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+const filePath =
+  process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
 
 function solution() {
@@ -7,19 +8,21 @@ function solution() {
   let arr = [];
   for (let i = 1; i <= num; i++) {
     arr.push(input[i].split(' ').map(Number));
-    console.log(arr);
   }
-  console.log(arr.sort((a, b) => a[1] - b[1]));
-  console.log(arr);
-  let et = 0;
-  let cnt = 0;
-
-  arr.forEach((v) => {
-    console.log(v);
-    if (v[0] >= et) {
-      et = v[1];
+  arr.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a[0] - b[0];
+    }
+    return a[1] - b[1];
+  });
+  let endTime = arr[0][1];
+  let cnt = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i][0] >= endTime) {
+      endTime = arr[i][1];
       cnt++;
     }
-  });
+  }
+  console.log(cnt);
 }
 solution();
