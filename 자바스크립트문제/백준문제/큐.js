115f257d8aa728df0994f1bd6b3fe82b4139b4d4
vjvl95 <vjvl95@naver.com>
2023-01-27 14:39:41 +0900

    sovled 큐

--- 자바스크립트문제/백준문제/큐.js
+++ 자바스크립트문제/백준문제/큐.js
@@ -1,27 +1,29 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : __dirname+"/input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
 
-function solution(){
-    let result=[]
-    let num=+input[0]
-    let answer=[]
-    for(let i=1;i<=num;i++){
-        input[i].split(" ").length===1?answer.push(length_1(input[i])):length_2(input[i])
-    }
-function length_1(input){
-    input=input.trim()
-    if(input==="size") return result.length
-    if(input==="empty") return result.length===0?1:0
-    if(input==="front") return result.length===0?-1:result[0]
-    if(input==="back")  return result.length===0?-1:result[result.length-1]
-    if(input==="pop")   return result.length===0?-1:result.shift()
-}
-function length_2(input){
-    input=input.split(" ")
-    result.push(+input[1])    
-}
-console.log(answer.join("\n"))
+function solution() {
+  let result = [];
+  let num = +input[0];
+  let answer = [];
+
+  for (let i = 1; i <= num; i++) {
+    input[i].split(' ').length === 1 ? answer.push(length_1(input[i])) : length_2(input[i]);
+  }
+
+  function length_1(input) {
+    if (input === 'size') return result.length;
+    if (input === 'empty') return result.length === 0 ? 1 : 0;
+    if (input === 'front') return result.length === 0 ? -1 : result[0];
+    if (input === 'back') return result.length === 0 ? -1 : result[result.length - 1];
+    if (input === 'pop') return result.length === 0 ? -1 : result.shift();
+  }
+
+  function length_2(input) {
+    input = input.split(' ');
+    result.push(+input[1]);
+  }
+  console.log(answer.join('\n'));
 }
 
-solution()
+solution();
