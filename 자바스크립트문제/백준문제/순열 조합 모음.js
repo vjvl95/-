c25da50065faa82b91ccca0406cd5dfa065ff8cf
vjvl95 <vjvl95@naver.com>
2023-02-22 20:35:32 +0900

    단일연결리스트 구현

---
+++ 자바스크립트문제/백준문제/순열 조합 모음.js
@@ -0,0 +1,45 @@
+// 순열
+function permutation(arr, selectNum) {
+  let result = [];
+  if (selectNum === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const fixer = v;
+    const restArr = arr.filter((_, index) => index !== idx);
+    const permuationArr = permutation(restArr, selectNum - 1);
+    const combineFixer = permuationArr.map((v) => [fixer, ...v]);
+    result.push(...combineFixer);
+  });
+  return result;
+}
+
+//  조합
+function combination(arr, selectNum) {
+  const result = [];
+  if (selectNum === 1) return arr.map((v) => [v]);
+  arr.forEach((v, idx, arr) => {
+    const fixed = v;
+    const restArr = arr.slice(idx + 1);
+    const combinationArr = combination(restArr, selectNum - 1);
+    const combineFix = combinationArr.map((v) => [fixed, ...v]);
+    result.push(...combineFix);
+  });
+  return result;
+}
+
+//  중복순열
+function permutation(arr, selectNum) {
+  const result = [];
+  if (selectNum === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, _, arr) => {
+    const fixed = v;
+    const restArr = arr;
+    const permutationArr = permutation(restArr, selectNum - 1);
+    const combineFix = permutationArr.map((v) => [fixed, ...v]);
+    result.push(...combineFix);
+  });
+  return result;
+}
+
+console.log(...getPermutations([1, 2, 3, 4], 4));
