7f28e6af9161b3c3f87a9f3fa130036d163687ad
vjvl95 <vjvl95@naver.com>
2023-03-02 14:09:15 +0900

    sovled 문자열나누기

---
+++ 자바스크립트문제/프로그래머스문제/문자열나누기.js
@@ -0,0 +1,28 @@
+function solution(s) {
+  var answer = 0;
+  let xCount = 1;
+  let otherCount = 0;
+  let i = 1;
+  while (s.length) {
+    let currentWord = s[0];
+
+    if (currentWord === s[i]) {
+      xCount++;
+    } else {
+      otherCount++;
+    }
+    if (xCount === otherCount) {
+      answer++;
+      s = s.slice(i + 1);
+      i = 1;
+      xCount = 1;
+      otherCount = 0;
+    } else {
+      i++;
+    }
+  }
+  console.log(answer);
+  return answer;
+}
+
+solution('aaabbaccccabba');
