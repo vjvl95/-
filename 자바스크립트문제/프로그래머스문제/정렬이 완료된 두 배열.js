fa048dd75a86feebbf91903647236dad82bc50a3
vjvl95 <vjvl95@naver.com>
2024-09-20 21:15:58 +0900

    sovled 정렬이 완료된 두 배열

---
+++ 자바스크립트문제/프로그래머스문제/정렬이 완료된 두 배열.js
@@ -0,0 +1,28 @@
+function soloution(arr1, arr2) {
+  const result = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      result.push(arr2[j]);
+      j++;
+    } else {
+      result.push(arr1[i]);
+      i++;
+    }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  console.log(result);
+}
+
+soloution([1, 2, 3, 9, 10, 35], [4, 5, 6, 39, 45]);
